8fb34b1d1c712117ea09e68c97411ec9b1959215
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-11-26 01:22:26 -0500

    NPM & API Setup

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
+const requirejs = require('requirejs');
+
 requirejs.config({
     baseUrl: 'vendors',
-    paths: {
-        'jquery': 'jquery-3.6.0.min',
-        'bootstrap-css': 'bootstrap.min',
-        'bootstap-js': 'bootstrap.min'
-    }
+    // paths: {
+    //     'jquery': 'jquery-3.6.0.min',
+    //     'bootstrap-css': 'bootstrap.min',
+    //     'bootstap-js': 'bootstrap.min'
+    // },
+    nodeRequire: require,
 });
 
 requirejs(['../lib/main']);
